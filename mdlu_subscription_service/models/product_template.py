7e0d83f6319df32dcf9c07a675cee30d61bc50e3
cmeravi <cmeravi@modfuelwholesale.com>
2020-06-28 15:04:48 -0400

    [IMP] fix upload

--- mdlu_subscription_service/models/product_template.py
+++
@@ -1,9 +0,0 @@
-# -*- coding: utf-8 -*-
-
-from odoo import models, fields, api
-
-class ProductTemplate(models.Model):
-    _inherit = "product.template"
-
-    po_subscription = fields.Boolean('Purchase Subscription')
-    so_subscription = fields.Boolean('Sale Subscription')
